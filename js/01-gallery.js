8082c518b3e52fb92459f73b7347ecb7be8fa478
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-02-21 00:48:46 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map((item) => {
+    let itemMarkup = `<div class = "gallery__item">
+		<a class="gallery__link" href=${item.original}>
+			<img
+				class="gallery__image"
+				src=${item.preview}
+				data-source=${item.original}
+				alt=${item.description}
+			/>
+		</a>
+	</div>`;
+    return itemMarkup;
+  })
+  .join("");
+
+gallery.innerHTML = markup;
+
+let instance;
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName != "IMG") {
+    return;
+  } else {
+    const linkOriginal = event.target.dataset.source;
+
+    instance = basicLightbox.create(`
+			<img src="${linkOriginal}">
+		`);
+    instance.show();
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key != "Escape") return;
+
+  if (instance) {
+    instance.close();
+  }
+});
