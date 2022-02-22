ea509eb6308dae644423b6f7a31f6f0fe34de1b4
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-02-23 00:49:51 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map((item) => {
+    let itemMarkup = `<a class="gallery__item" href=${item.original}>
+			  <img class="gallery__image" src=${item.preview} alt=${item.description} />
+			</a>`;
+    return itemMarkup;
+  })
+  .join("");
+
+gallery.innerHTML = markup;
+
+const lightbox = new SimpleLightbox(".gallery .gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
